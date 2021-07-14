5d3caac891fdf752d54909caa407abc51608eb82
Abhishek <abhishekvihan2@gmail.com>
2021-07-14 17:37:37 +0530

    created empty pages

---
+++ src/pages/Recording.page.tsx
@@ -0,0 +1,11 @@
+import { FC, memo } from 'react';
+
+interface Props {}
+
+const Recording: FC<Props> = (props) => {
+  return <div>This is Recording page.</div>;
+};
+
+Recording.defaultProps = {};
+
+export default memo(Recording);
